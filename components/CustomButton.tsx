d7a30eca12797808e4bcf16aa9e401f379a8f1ee
Umair Qaisar <umairqaisar@Umairs-MacBook-Pro.local>
2024-10-14 22:18:34 +0500

    feat: initial setup and hero section code

---
+++ components/CustomButton.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+
+const CustomButton = () => {
+    return (
+        <button
+            disabled={false}
+            type={"button"}
+            className={`custom-btn`}
+            onClick={() => { }}
+        >
+            <span className={`flex-1`}>
+                title
+            </span>
+        </button>
+    )
+}
+
+export default CustomButton
