f08311980dcbf186dcd2fefb18c3d5742ca4b1a7
Umair Qaisar <umairqaisar@Umairs-MacBook-Pro.local>
2024-10-15 21:57:43 +0500

    feat: added hero section and some assets images

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,7 +1,13 @@
+"use client"
+import Image from 'next/image'
 import React from 'react'
 import CustomButton from "./CustomButton"
 
 const Hero = () => {
+    const handleScroll = () => {
+
+    }
+
     return (
         <div className="hero">
             <div className="flex-1 pt-36 padding-x">
@@ -12,7 +18,17 @@ const Hero = () => {
                     Streamline your car rental experience with our effortless booking process.
                 </p>
 
-                <CustomButton />
+                <CustomButton
+                    title="Explore Cars"
+                    containerStyles="bg-primary-blue text-white rounded-full mt-10"
+                    handleClick={handleScroll}
+                />
+            </div>
+            <div className='hero__image-container'>
+                <div className='hero__image'>
+                    <Image src="/hero.png" alt="hero" fill className='object-contain' />
+                    <div className='hero__image-overlay' />
+                </div>
             </div>
         </div>
     )
--- components/index.ts
+++ components/index.ts
@@ -1,7 +1,10 @@
 import CustomButton from "./CustomButton";
+import Footer from "./Footer";
 import Hero from "./Hero";
+import Navbar from "./Navbar";
+
 
 export {
-    CustomButton,
-    Hero
+    CustomButton, Footer, Hero, Navbar
 };
+
